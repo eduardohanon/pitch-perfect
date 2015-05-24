d98bba252ddb4b3614373bdbdf62cf22ffe0232c
eduardohanon <hanon2@illinois.edu>
2015-05-23 21:42:39 -0500

    Committed by Udacity

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Eduardo Hanón on 5/23/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject
+    {
+    var filePathUrl: NSURL!
+    var title: String!
+    }
